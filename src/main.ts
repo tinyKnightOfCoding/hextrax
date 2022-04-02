779d5c052bd64f1f1ae2d625a843709678af1b86
Kenneth Radunz <kenneth.radunz@protonmail.ch>
2022-04-02 15:28:44 +0200

    removes semicolon

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import {defaultMode, demoMode} from "./mode";
 
 const mode = (new URLSearchParams(window.location.search).get("mode") ?? "default").toLowerCase()
 
-const simulation = new Simulation(document.querySelector<HTMLCanvasElement>('#app')!!);
+const simulation = new Simulation(document.querySelector<HTMLCanvasElement>('#app')!!)
 
 switch (mode) {
     case 'demo':
