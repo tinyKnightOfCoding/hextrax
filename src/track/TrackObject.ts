b733314d337c4276bc6a3fed7447a3ceee89e78f
Kenneth Radunz <kenneth.radunz@protonmail.ch>
2022-04-12 20:16:27 +0200

    makes curves smoother

--- src/track/TrackObject.ts
+++ src/track/TrackObject.ts
@@ -1,47 +1,75 @@
-import {AbstractMesh, Mesh, MeshBuilder, Node, Scene, StandardMaterial, Vector3} from "@babylonjs/core";
-import {Direction} from "../grid";
-import {defaultTrackObjectOptions, TrackObjectOptions} from "./TrackObjectOptions";
+import {AbstractMesh, Mesh, MeshBuilder, Node, Scene, StandardMaterial, Vector3} from '@babylonjs/core'
+import {Direction} from '../grid'
+import {defaultTrackObjectOptions, TrackObjectOptions} from './TrackObjectOptions'
 
 export class TrackObject {
-    private readonly fromHalf: Mesh
-    private readonly toHalf: Mesh
+    private readonly meshes: Mesh[] = []
 
     constructor(
         from: Direction,
         to: Direction,
         scene: Scene,
         parent: Node,
-        options: Partial<TrackObjectOptions> = {}
+        options: Partial<TrackObjectOptions> = {},
     ) {
+        this.createMeshes(options, scene, parent, from, to)
+        if (options.actionManager) {
+            this.meshes.forEach(m => m.actionManager = options.actionManager!!)
+        }
+    }
+
+    private createMeshes(options: Partial<TrackObjectOptions>, scene: Scene, parent: Node, from: Direction, to: Direction) {
         const {color, alpha, width, height} = {...defaultTrackObjectOptions(), ...options}
-        const mat = new StandardMaterial("gray", scene)
+        const mat = new StandardMaterial('gray', scene)
         mat.diffuseColor = color
         mat.specularColor = color
         mat.alpha = alpha
-        this.fromHalf = MeshBuilder.CreateBox("track", {width: width, height: height, depth: Math.sqrt(3) / 4}, scene)
-        this.fromHalf.material = mat
-        this.fromHalf.parent = parent
-        this.fromHalf.translate(Vector3.Up(), 0.105)
-        this.fromHalf.translate(from.direction, Math.sqrt(3) / 8)
-        this.fromHalf.rotation = from.rotation
-        this.toHalf = MeshBuilder.CreateBox("track", {width: width, height: height, depth: Math.sqrt(3) / 4}, scene)
-        this.toHalf.material = mat
-        this.toHalf.parent = parent
-        this.toHalf.translate(Vector3.Up(), 0.105)
-        this.toHalf.translate(to.direction, Math.sqrt(3) / 8)
-        this.toHalf.rotation = to.rotation
-        if (options.actionManager) {
-            this.toHalf.actionManager = options.actionManager
-            this.fromHalf.actionManager = options.actionManager
+        if(from.opposite === to) {
+            const m = MeshBuilder.CreateBox('track', {width: width, height: height, depth: Math.sqrt(3) / 2}, scene)
+            m.material = mat
+            m.parent = parent
+            m.translate(Vector3.Up(), 0.105)
+            m.rotation = from.rotation
+            this.meshes.push(m)
+        } else {
+            const fromH = MeshBuilder.CreateBox('track', {width: width, height: height, depth: Math.sqrt(3) / 6}, scene)
+            fromH.material = mat
+            fromH.parent = parent
+            fromH.translate(Vector3.Up(), 0.105)
+            fromH.translate(from.direction, Math.sqrt(3) / 6)
+            fromH.rotation = from.rotation
+            const toH = MeshBuilder.CreateBox('track', {width: width, height: height, depth: Math.sqrt(3) / 6}, scene)
+            toH.material = mat
+            toH.parent = parent
+            toH.translate(Vector3.Up(), 0.105)
+            toH.translate(to.direction, Math.sqrt(3) / 6)
+            toH.rotation = to.rotation
+
+            const middleH = MeshBuilder.CreateBox('track', {width: width, height: height, depth: Math.sqrt(3) / 6}, scene)
+            middleH.material = mat
+            middleH.parent = parent
+            const fromPos = fromH.position
+            const toPos = toH.position
+            middleH.position = new Vector3((fromPos.x + toPos.x) / 4, 0.105, (fromPos.z + toPos.z) / 4)
+            let yRot = (from.rotation.y - to.rotation.y)
+            // fix two combinations :D
+            if((to === Direction.NORTH_EAST && from === Direction.SOUTH_EAST) || (to === Direction.NORTH_WEST && from === Direction.SOUTH_WEST)) {
+                yRot = Math.PI
+            }
+            middleH.rotation = new Vector3(0, yRot, 0)
+            middleH.rotation = middleH.rotation.add(new Vector3(0, Math.PI / 2, 0))
+
+            this.meshes.push(fromH)
+            this.meshes.push(toH)
+            this.meshes.push(middleH)
         }
     }
 
     hasMesh(mesh: AbstractMesh): boolean {
-        return this.fromHalf === mesh || this.toHalf === mesh
+        return this.meshes.some(m => m === mesh)
     }
 
     dispose() {
-        this.fromHalf.dispose()
-        this.toHalf.dispose()
+        this.meshes.forEach(m => m.dispose())
     }
 }
